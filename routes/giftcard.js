947059d211acabe7847051240f64336f0f1aadd5
alexvogelKIT <vogelalex2000@gmail.com>
2020-03-21 17:14:31 +0100

    gift card info

--- routes/giftcard.js
+++ routes/giftcard.js
@@ -3,15 +3,29 @@ const sequalize = require('sequelize');
 const GiftCard = require('../models/GiftCard');
 
 
-router.post('/giftcard/delete', async(req, res) => {
+router.post('/delete', async(req, res) => {
     await GiftCard.destroy({
         where: {
-            id: req.body.giftCardId
+            id: req.body.id
         }
     }).then(function(rowDeleted){
                 res.status(200);
-            }).catch(function(err){
+        }).catch(function(err){
                 res.status(400);
-            })
-    });
+    })
 });
+
+router.get('/info', async(req, res) => {
+    var giftCard = await GiftCard.findAll({
+        where: {
+            id = req.id
+        }
+    });
+    
+    if (giftCard == null){
+        res.status(404);
+    }
+    res.status(200).send(JSON.stringify(giftCard));
+});
+
+module.exports = router;
