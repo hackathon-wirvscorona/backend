157ce75c7958148da77812543545eeacf4bfc10c
alexvogelKIT <vogelalex2000@gmail.com>
2020-03-21 12:41:36 +0100

    combined

---
+++ routes/company.js
@@ -0,0 +1,51 @@
+
+const router = require('express').router();
+const sequalize = require('sequelize');
+const Company = require('../models.Company');
+var fuzzy = require('fuzzyset.js');
+
+/**
+ * returns all companies near the user
+ */
+router.get('/searchDistance', async(req, res) => {
+    var companieslist = await Company.findAll();
+    var longPos = req.longitude;
+    var latPos = req.latitude;
+    var companiesNearby = [];
+    var maxDistance = 10;
+    
+    companieslist.array.forEach(element => {
+        var dx = 71.3 * (longPos - element.longitude);
+        var dy = 111.3 * (latPos - element.latitude);
+        var distance = Math.sqrt(dx * dx + dy * dy);
+        if (distance < maxDistance){
+            companiesNearby.push(element);
+        }
+    });
+
+    res.status(200).send(JSON.stringify(companiesNearby));
+
+});
+
+router.get('/searchName', async(req, res) => {
+    var content = req.content;
+    var companies = await Company.findAll();
+    var companyNames = [];
+    var companyDict;
+    companies.array.forEach(element => {
+        companyNames.push(element.name);
+        companyDict[element.name] = element;
+    });
+
+    var a = FuzzySet(companyNames);
+
+    var result;
+    var fuzzy = a.get(content).forEach(element => {
+        result = companyDict[element];
+    });
+    
+
+    res.status(200).send(JSON.stringify(result));
+});
+
+module.exports(router);
