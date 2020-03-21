ce478dd9e17320369aeb9ccdd956d4db23787163
Louis Kronberg <louiskronberg@gmail.com>
2020-03-21 12:20:39 +0100

    add Company and Giftcard model
    This also adds Associations between Giftcard-, Company-, Usermodel.
    There might be a better way to save the geodata in Company.
    TODO: complete fields for all models

---
+++ models/Company.js
@@ -0,0 +1,36 @@
+const GiftCard = require('./GiftCard')
+const Sequelize = require('sequelize');
+const db = require('../config/database');
+
+const Company  = db.define('company', {
+    name: {
+        type: Sequelize.STRING,
+        unique: true,
+        require: true,
+        min: 6,
+        max: 255,
+    },
+
+    adress: {
+        type: Sequelize.STRING,
+        require: true,
+        min: 6,
+        max: 255,
+    },
+
+    longitude: {
+        type: Sequelize.DOUBLE,
+        require: true,
+    },
+        
+    latitude: {
+        type: Sequelize.DOUBLE,
+        require: true,
+    },
+
+})
+
+
+Company.hasMany(Giftcard, {foreignKey: 'companyFk', targetKey:'id'});
+module.exports = company;
+
