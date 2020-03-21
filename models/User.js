9705610552116edbd1c806ace49ea2c90518f574
lordimperrator <mzingg@freenet.de>
2020-03-21 11:31:57 +0100

    Added user model

---
+++ models/User.js
@@ -0,0 +1,26 @@
+const Sequelize = require('sequelize');
+const db = require('../config/database');
+
+const User = db.define('user', {
+    name: {
+        type: Sequelize.STRING,
+        unique: true,
+        require: true,
+        min: 6,
+        max: 255,
+    },
+    email: {
+        type: Sequelize.STRING,
+        require: true,
+        min: 6,
+        max: 255,
+    },
+    password: {
+        type: Sequelize.STRING,
+        require: true,
+        min: 6,
+        max: 1024,
+    },
+})
+
+module.exports = User;
