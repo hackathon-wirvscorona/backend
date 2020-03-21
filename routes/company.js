f2c31e339d0dc70b1e3e7e34a23343dfd915571a
Louis Kronberg <louiskronberg@gmail.com>
2020-03-21 21:32:58 +0100

    add post, get, delete /offer and rewrite /createOffer

--- routes/company.js
+++ routes/company.js
@@ -55,25 +55,84 @@ router.get('/searchName', async(req, res) => {
     res.status(200).send(JSON.stringify(result));
 });
 
+// TODO: Implement as stated in
 router.get('/offers', verify, async(req, res) => {
-    var user = await User.findAll({
-        where: {
-            name: req.body.name
-        }
-    });
-
-    var list = user.getOffers();
+    var list = Offer.findAll();
     res.status(200).send(JSON.stringify(list));
 });
 
+// FIXME: this only gives 400 
 router.post('/createOffer', verify, async(req, res) => {
-    offer = await Offer.create({
+    Offer.create(
+    {
         name: req.body.name,
         description: req.body.description,
         min_value: req.body.min_value,
         max_value: req.body.max_value,
         companyFk: req.user
-    }).catch(err => (res.status(400).send(err)));    
+    })
+    .then(offer =>(res.status(200).send(offer)))
+    .catch(err => (res.status(400).send(err)));    
+});
+
+router.get('/offer/:id', verify, async(req, res) => {
+    offer = await Offer.findOne({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(offer =>{
+        if(offer){
+            // found offer
+            res.status(200).send(offer);
+        }else{
+            res.status(404); // not found
+        }
+    })
+    .catch(err => (res.status(400).send(err)));    
+});
+
+
+router.post('/offer/:id', verify, async(req, res) => {
+    offer = await Offer.update(
+    {
+        name: req.body.name,
+        description: req.body.description,
+        min_value: req.body.min_value,
+        max_value: req.body.max_value,
+        companyFk: req.user
+    }, {
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(offer => {
+        if(offer){
+            // found offer
+            res.status(200).send(offer);
+        }else{
+            res.status(404); // not found
+        }
+    })
+    .catch(err => (res.status(400).send(err)));    
+
+});
+
+router.delete('/offer/:id', verify, async(req, res) => {
+    offer = await Offer.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(offer =>{
+        if(offer){
+            // found offer
+            res.status(200).send(offer);
+        }else{
+            res.status(404); // not found
+        }
+    })
+    .catch(err => (res.status(400).send(err)));    
 
     res.status(200).json(offer);
 });
